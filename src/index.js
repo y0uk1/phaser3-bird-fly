e464b4f0eac44af5aa810136388b018fdf850c07
Yuki Ueno <y0uk10126@gmail.com>
2023-04-08 13:09:22 +0900

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,28 +1,36 @@
+import Phaser from 'phaser';
 
-import Phaser from "phaser";
+import PlayScene from './scenes/PlayScene';
+import MenuScene from './scenes/MenuScene';
+import PreloadScene  from './scenes/PreloadScene';
+import ScoreScene from './scenes/ScoreScene';
+import PauseScene from './scenes/PauseScene';
+
+const WIDTH = 400;
+const HEIGHT = 600;
+const BIRD_POSITION = {x: WIDTH * 0.1, y: HEIGHT / 2};
+
+const SHARED_CONFIG = {
+  width: WIDTH,
+  height: HEIGHT,
+  startPosition: BIRD_POSITION
+}
+
+const Scenes = [PreloadScene, MenuScene, PlayScene, ScoreScene, PauseScene];
+const createScene = Scene => new Scene(SHARED_CONFIG);
+const initScenes = () => Scenes.map(createScene);
 
 const config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  ...SHARED_CONFIG,
+  pixelArt: true,
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 200 }
+      // debug: true,
     }
   },
-  scene: {
-    preload: preload,
-    create: create
-  }
-};
+  scene: initScenes()
+}
 
 new Phaser.Game(config);
-
-function preload () {
-  this.load.image('sky', 'assets/sky.png');
-}
-
-function create () {
-  this.add.image(400, 300, 'sky');
-}
